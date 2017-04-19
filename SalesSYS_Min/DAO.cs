2b704a738fc910df5124fc4799a1b2c2576f42fe
inly4 <134679iL>
2017-04-19 17:47:16 +0100

    removed oracle reference in dao.cs

--- SalesSYS_Min/DAO.cs
+++ SalesSYS_Min/DAO.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Oracle.DataAccess.Client;
+
 
 using System.Windows.Forms;
 using MySql.Data.MySqlClient;
